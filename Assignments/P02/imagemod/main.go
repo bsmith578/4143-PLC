12f00fcb39258c0678503159942e95abfa5a3bd4
bsmith578 <brettsmith578@gmail.com>
2023-09-19 14:45:09 -0500

    created files

---
+++ Assignments/P02/imagemod/main.go
@@ -0,0 +1,26 @@
+// imageManipulator/main.go
+
+package main
+
+import (
+	"fmt"
+	"myimageapp/imagemod/imageManipulator"
+)
+
+func main() {
+	// Create an ImageManipulator instance
+	im := imageManipulator.NewImageManipulator(800, 600)
+
+	// Create an ImageManipulator instance with an existing image
+	im, err := imagemod.NewImageManipulatorWithImage("mustangs.jpg")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Draw a rectangle
+	im.DrawRectangle(150, 50, 560, 411)
+
+	// Save the image to a file
+	im.SaveToFile("mustangs.png")
+}
